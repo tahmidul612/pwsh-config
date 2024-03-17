50986b6957b2842e88a8154ef7b4e9c560c195be
tahmidul612 <tahmidul612@gmail.com>
2024-03-17 04:37:11 -0400

    Add DailyUpdate, update-check and update-all functions
    - Check winget and scoop for updates daily (runs only once every day)
    - update-all function manually runs the winget and scoop update commands
    - update-check manually checks for updates

--- profile.ps1
+++ profile.ps1
@@ -46,3 +46,42 @@ function scoop {
         }
     }
 }
+
+# Function to check winget and scoop for updates
+function update-check {
+    $ErrorActionPreference_bak = $ErrorActionPreference
+    $ErrorActionPreference = "SilentlyContinue"
+    Write-Host       "Checking for updates..." -ForegroundColor Gray -NoNewline
+    Invoke-Expression "winget update *>&1" -OutVariable winget | Out-Null
+    Invoke-Expression "scoop update *>&1" | Out-Null
+    Invoke-Expression "scoop status *>&1" -OutVariable scoop | Out-Null
+    if (($winget -like "*No installed package found matching input criteria.*") -and ($scoop -like "*Everything is ok!*")) {
+        Write-Host "`rNo updates available   " -ForegroundColor Green -NoNewline
+    }
+    else {
+        Write-Host "`rUpdates available      " -ForegroundColor Yellow -NoNewline
+        Write-Host "`nRun 'update-all' to update all packages" -ForegroundColor Gray
+    }
+    $ErrorActionPreference = $ErrorActionPreference_bak
+}
+
+# Run update-check on a daily basis
+function DailyUpdate {
+    trap { $last_date = $null }
+    Invoke-Expression "(Get-ItemProperty -Path HKCU:\Environment -Name LastUpdate).LastUpdate *>&1" -OutVariable last_date | Out-Null
+    $cur_date = Get-Date -Format "MM/dd/yyyy"
+    if ($last_date -eq $null -or !$last_date -or $last_date -ne $cur_date) {
+        Set-ItemProperty -Path HKCU:\Environment -Name LastUpdate -Value $cur_date
+        update-check
+    }
+    else {
+        Set-ItemProperty -Path HKCU:\Environment -Name LastUpdate -Value $cur_date
+    }
+}
+DailyUpdate
+
+# Update all packages
+function update-all {
+    winget update --all;
+    scoop update --all;
+}
